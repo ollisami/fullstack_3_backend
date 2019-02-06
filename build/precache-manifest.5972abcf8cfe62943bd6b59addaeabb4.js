fa598670c8c36ba74743ffe95f3d00b3b98b42a3
AR team <arteam@ARs-MacBook-Pro.local>
2019-02-06 10:00:51 +0200

    backend siirretty omaan repoon

---
+++ build/precache-manifest.5972abcf8cfe62943bd6b59addaeabb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d61df1c6b4b1a96ef0fe",
+    "url": "/static/js/main.d61df1c6.chunk.js"
+  },
+  {
+    "revision": "1bdeb50c48bfe976fdd4",
+    "url": "/static/js/1.1bdeb50c.chunk.js"
+  },
+  {
+    "revision": "d61df1c6b4b1a96ef0fe",
+    "url": "/static/css/main.1b779479.chunk.css"
+  },
+  {
+    "revision": "2b17c852d47fa6bf0fa1001f5246509c",
+    "url": "/index.html"
+  }
+];
